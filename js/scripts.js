1c178fa5cbff071e181bcb5bcd9fd3c66a0d8621
Yazmin Perez <zoeperez333@gmail.com>
2022-02-07 10:43:56 -0800

    add forms to html, display none to css, and js

---
+++ js/scripts.js
@@ -0,0 +1,93 @@
+
+// $(document).ready(function() {
+//   $("form#grocery").submit(function(event) {
+//     event.preventDefault();
+
+//     const fullName1Input = $("input#fullName").val();
+//     const itemOne = $("input#itemOne").val();
+//     const itemTwo = $("input#itemTwo").val();
+//     const itemThree = $("input#itemThree").val();
+
+//     console.log(fullName1Input);
+//     console.log(itemOne);
+//     console.log(itemTwo);
+//     console.log(itemThree);
+
+
+//     $(".fullName").text(fullName1Input);
+//     $(".itemOne").text(itemOne);
+//     $(".itemTwo").text(itemTwo);
+//     $(".itemThree").text(itemThree);
+
+//     $("#final").show();
+//     $("form#grocery").hide();
+
+ 
+//     let groceryList = [itemOne, itemTwo, itemThree];
+//     let itemList = groceryList.sort();
+//     const upperCase = [];
+
+//     itemList.forEach(function(item) {
+//       upperCase.push(item.toUpperCase());
+   
+//     });
+
+    
+//     upperCase.forEach(function(list) {
+//             $("#myList").append('<li>' + list + '</li>');
+//             // groceryList.push(itemOne, itemTwo, itemThree);
+
+// });
+// });
+// });
+
+
+
+// below is for the grocery list. User input food items, the items in the array become a list
+  
+   
+
+    
+
+    // groceryList.sort(); //must have sort below let groceryList to work
+// groceryList.sort();
+
+// groceryList.forEach(function( ) {
+
+// }
+
+    // let upperCase = groceryList.join('$$').toUpperCase().split('$$').sort();
+
+    // for (let i=0; i< groceryList.length; i++) {
+    //   upperCase[i].toUpperCase();
+    // }
+    // $(groceryList).toUpperCase();
+    // console.log(upperCase);
+  
+
+
+
+
+
+// const grocList = [];
+// grocList;
+    
+//     grocList.forEach(function(list) {
+//       grocList.push(list);
+//       $("#myList").append('<li>' + list + '</li>');
+
+
+// $("#final").slideDown()
+
+// $(document).ready(function() {
+//   $("form#formOne").submit(function(event) {
+//     event.preventDefault();
+    
+//     $("#formOne").hide(); // helps reload and displays a new page
+
+//   $("#final").click(function() {
+//     $("#formOne").slideDown();
+//     $("#final").hide();
+//   });
+// });
+// });
